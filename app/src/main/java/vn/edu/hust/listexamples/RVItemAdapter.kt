7137e8a9107878d367474878b47d8b3d12687129
hoang.danh.quan <hoangdanhquan48@gmail.com>
2023-11-01 14:39:26 +0700

    update email

---
+++ app/src/main/java/vn/edu/hust/listexamples/RVItemAdapter.kt
@@ -0,0 +1,11 @@
+package vn.edu.hust.listexamples
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RVItemAdapter {
+
+    class RVItemViewHolder(val itemView: View): RecyclerView.ViewHolder(itemView){
+        val
+    }
+}
